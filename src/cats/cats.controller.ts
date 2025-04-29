098cf69042386e1136b84d03ff0f22bbda9ace24
hammadurrehman-dev <hammad.desopers@gmail.com>
2025-04-29 17:42:19 +0500

    feat: add findAll method to CatsController for retrieving all cats

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  @Get()
+  findAll(@Req() request: Request) {}
+}
