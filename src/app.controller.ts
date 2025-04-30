d7391cee1555217d210c26d063b84c54658f4a37
hammadurrehman01 <hammadurrehman@techlabs.cc>
2025-04-30 07:32:33 +0500

    feat: update AppController to include new POST endpoints with status code and headers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/practice')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/withstatuscode')
+  @HttpCode(207)
+  withStatusCode() {
+    return 'Return the status code';
+  }
+
+  @Post('/withheaders')
+  @Header('Cache-Control', 'cache')
+  withHeaders() {
+    return 'No store cache in the headers';
   }
 }
